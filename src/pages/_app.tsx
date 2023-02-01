78a34f1a634d480e38cf7f433e76569a65b6214b
Siva Addanki <34810623+avsssai@users.noreply.github.com>
2023-02-01 13:27:55 +0530

    added @next font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Merriweather } from "@next/font/google";
+
+const merriweather = Merriweather({
+	variable: "--font-merriweather",
+	weight: ["300", "400", "700"],
+	subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<main className={`${merriweather.variable} font-sans`}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
